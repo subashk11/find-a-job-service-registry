9a0413f50dfd192e52edb7008f8bbdfaea2bac5e
subashk11 <subashk1153@gmail.com>
2024-09-03 00:24:10 +0530

    Created Service registry to communicate between microservices

---
+++ src/test/java/com/springboot/microservices/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.microservices.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
